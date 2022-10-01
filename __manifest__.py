579b9759ec290effdfd44480bcf3d45ae9062459
USER <yarmiztechnlogies@gmail.com>
2022-10-01 13:17:43 +0530

    <MounikEnzapps>

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Base UBL Inherit",
+    "version": "14.0.1.1.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "summary": "Base module for Universal Business Language (UBL)",
+    "author": "Akretion,Onestein,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["uom_unece", "account_tax_unece", "base_vat","base_ubl","base_ubl_payment"],
+    "data": ["views/unece_code_list.xml"],
+    "external_dependencies": {"python": ["PyPDF2"]},
+    "installable": True,
+}
